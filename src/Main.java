1211a7c297f9c2376bc046a1713be9fc770004c0
Ryan Earl <ryanleoearl@gmail.com>
2020-04-26 22:44:20 -0700

    Second try

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        EmployeeList el = new EmployeeList();
+        System.out.println();
+        System.out.println(el.EmployeeList());
+        System.out.println();
+    }
+}
